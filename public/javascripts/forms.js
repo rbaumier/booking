4f1a925025035dd0a2cda3490e9fdb0e19c14c33
Romain Baumier <baumier.romain@gmail.com>
2015-04-14 19:18:53 +0200

    refactor(useless)

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -1,9 +1,11 @@
-// DEBUG: autofill date-time: $('#start_date').val("2015-04-14T15:00");
+// DEBUG: autofill date-time:
+$('#start_date').val("2015-04-14T15:00");
 
-$('.delete-button').click(function(e) {
+$('.delete-button').click(function (e)  {
   e.preventDefault();
 });
 
 $('#confirm-delete').on('show.bs.modal', function (e) {
   $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
 });
+
