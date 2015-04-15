9ac1beb98c005f82162f92dbae9384d32f4d5af7
Romain Baumier <baumier.romain@gmail.com>
2015-04-15 09:57:27 +0200

    feat(own twitter bootstrap for constructor)

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -1,6 +1,6 @@
-// DEBUG: autofill date-time:
-$('#start_date').val("2015-04-14T15:00");
+$('#start_date').val("2015-04-15T15:00"); // DEBUG: autofill date-time
 
+// modal to confirm deletion
 $('.delete-button').click(function (e)  {
   e.preventDefault();
 });
@@ -8,4 +8,7 @@ $('.delete-button').click(function (e)  {
 $('#confirm-delete').on('show.bs.modal', function (e) {
   $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
 });
+
+// dynamic form (add/delete lines with +/- buttons
+
 
